0b758c984c40953a7ef6ec891f0ac2daa4b218bd
Frederico Maia Arantes <fredmaiaarantes@gmail.com>
2022-01-28 19:45:08 -0300

    - Fix accessibility and SEO issues

--- app/main.client.js
+++ app/main.client.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { Routes } from './ui/common/Routes';
+import { Spinner } from '@chakra-ui/react';
 
 /**
  * This is the client-side entry point
@@ -10,7 +11,7 @@ Meteor.startup(() => {
   document.documentElement.setAttribute('lang', 'en');
   const rootElement = document.getElementById('react-target');
   render(
-    <Suspense fallback={<span>Loading...</span>}>
+    <Suspense fallback={<Spinner />}>
       <Routes />
     </Suspense>,
     rootElement
