a974d171ab51d64357175c5af6607599b987c632
Frederico Maia <fredmaiaarantes@gmail.com>
2023-12-04 12:11:20 -0300

    Temporarily add `fredericomaia:easy-schema`

--- api/tasks/tasks.js
+++ api/tasks/tasks.js
@@ -2,12 +2,12 @@ import { Mongo } from 'meteor/mongo';
 
 export const Tasks = new Mongo.Collection('tasks');
 
-// const schema = {
-//   _id: String,
-//   description: String,
-//   done: Boolean,
-//   createdAt: Date,
-//   userId: String,
-// };
-//
-// Tasks.attachSchema(schema);
+const schema = {
+  _id: String,
+  description: String,
+  done: Boolean,
+  createdAt: Date,
+  userId: String,
+};
+
+Tasks.attachSchema(schema);
